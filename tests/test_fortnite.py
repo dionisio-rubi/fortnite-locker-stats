2f1d12ced6eb9ce0825279f4d0758aad3ac40d3d
dionisio-rubi <rdionisio@nevada.unr.edu>
2024-05-07 20:26:54 -0700

    added docker

---
+++ tests/test_fortnite.py
@@ -0,0 +1,10 @@
+import unittest
+
+
+class CombinationControllerTest(unittest.TestCase):
+    def test_comboName(self):
+        self.assertEqual('Fortnite', 'Fortnite')
+
+
+if __name__ == '__main__':
+    unittest.main()
